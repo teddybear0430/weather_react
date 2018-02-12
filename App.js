44ed5b309150ef58d3bc9da3769c2501f46bd733
teddybear0430 <teddybear0430@hotmail.com>
2018-02-12 10:51:11 -0800

    Added thre text fields for location, weather, and temperature. Adding styling to all three fieids. Included a text input for users to enter location.

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  View,
+  Platform,
+  TextInput,
+ } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+export default class App extends React.Component { render() {
+return (
+<View style={styles.container}>
+    <Text style={[styles.largeText, styles.textStyle]}>Vancouver</Text>
+    <Text style={[styles.smallText, styles.textStyle]}>Sunny</Text>
+    <Text style={[styles.largeText, styles.textStyle]}>4°</Text>
 
-const styles = StyleSheet.create({
-  container: {
+    <TextInput
+      autoCorrect={false}
+      placeholder="Search any city" placeholderTextColor="white" style={styles.textInput} clearButtonMode="always"/>
+</View> );
+} }
+
+
+const styles = StyleSheet.create({ container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
   },
 });
