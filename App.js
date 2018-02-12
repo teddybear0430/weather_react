39def6d86b40d786c7862f67578fdb5fb4bf0832
teddybear0430 <teddybear0430@hotmail.com>
2018-02-12 12:00:53 -0800

    Added imagebackground within render and imported getImageForWeather

--- App.js
+++ App.js
@@ -4,36 +4,50 @@ import {
   Text,
   KeyboardAvoidingView,
   View,
+  ImageBackground,
   Platform,
-  TextInput,
  } from 'react-native';
 
+import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
+
 
 
 export default class App extends React.Component {
   render() {
+    const location = 'Vancouver';
+
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <Text style={[styles.largeText, styles.textStyle]}>Vancouver</Text>
-        <Text style={[styles.smallText, styles.textStyle]}>Sunny</Text>
-        <Text style={[styles.largeText, styles.textStyle]}>4°</Text>
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
 
-        <SearchInput placeholder="Search any city" />
-      </KeyboardAvoidingView> );
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>Sunny</Text>
+            <Text style={[styles.largeText, styles.textStyle]}>4°</Text>
+
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
   }
 }
 
 
-const styles = StyleSheet.create({ container: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E',
   },
   textStyle: {
     textAlign: 'center',
     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
@@ -41,14 +55,19 @@ const styles = StyleSheet.create({ container: {
   smallText: {
     fontSize: 18,
   },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+   flex: 1,
+   justifyContent: 'center',
+   backgroundColor: 'rgba(0,0,0,0.2)',
+   paddingHorizontal: 20,
   },
 });
